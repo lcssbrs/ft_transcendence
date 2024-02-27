14bb24c66f5f53ec44d30ab52c6a5808d6c0eec7
NineSuper <NineSuper66@gmail.com>
2024-02-27 20:18:47 +0100

    fix debug mode Django

--- app/manage.py
+++ app/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run istrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
     try:
         from django.core.management import execute_from_command_line
