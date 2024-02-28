25b7e93cdc500cb7fb25c96099445dd50b9b55ce
lcssbrs <lucas.seib@icloud.com>
2024-02-28 16:51:40 +0100

    test for django but not working connction failed

--- app/hello_django/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class usertest(models.Model):
-	login = models.TextField()
-	first_name = models.TextField()
-	last_name = models.TextField()
-	image = models.TextField()
