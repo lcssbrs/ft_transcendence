9c927136933355a8e6a2758ed9895b292b53073d
NineSuper <NineSuper66@gmail.com>
2024-02-29 14:52:38 +0100

    Add Adminer Docker-compose

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
