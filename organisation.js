43c44e00be19f9ad7cbfa8b3aa02223ee37a4d9e
hbaduel <hbaduel@student.42perpignan.fr>
2024-02-29 12:08:35 +0100

    profile + friend bar + some adjustements

--- organisation.js
+++ organisation.js
@@ -79,3 +79,35 @@ window.addEventListener('popstate', function(event) {
 	else if (state.affichage == 'classement')
 		Page6();
 });
+
+// import { Input, Ripple, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Input, Ripple });
+
+// const searchFocus = document.getElementById('search-focus');
+// const keys = [
+// 	{ keyCode: 'AltLeft', isTriggered: false },
+// 	{ keyCode: 'ControlLeft', isTriggered: false },
+// ];
+
+// window.addEventListener('keydown', (e) => {
+// 	keys.forEach((obj) => {
+// 		if (obj.keyCode === e.code) {
+// 		obj.isTriggered = true;
+// 	}
+// });
+
+//   const shortcutTriggered = keys.filter((obj) => obj.isTriggered).length === keys.length;
+
+// 	if (shortcutTriggered) {
+// 		searchFocus.focus();
+// 	}
+// });
+
+// window.addEventListener('keyup', (e) => {
+// 	keys.forEach((obj) => {
+// 	if (obj.keyCode === e.code) {
+// 		obj.isTriggered = false;
+// 	}
+// 	});
+// });
