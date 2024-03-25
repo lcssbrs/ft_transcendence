d244f52f224211ae4789416d5e6a1885e0679d75
hbaduel <hugo.baduel81@gmail.com>
2024-03-25 14:41:59 +0100

    front of qrcode

--- app/static/js/friend.js
+++ app/static/js/friend.js
@@ -21,22 +21,22 @@ $(document).ready(function() {
                     var $statusIcon = $('<div class="status-icon mr-2" style="width: 10px; height: 10px; border-radius: 50%; background-color: ' + statusColor + ';"></div>');
 
                     var $friendElement = $('<div class="pt-1 mb-2 d-flex justify-content-between mr-3">' +
-                        '<div class="d-flex align-items-center">' +
-                        '<a href="/profile/?id=' + friend.id + '" class="text-white redir">' +
-                        '<span>' + friend.username + '</span>' +
-                        '</a>' +
-                        '</div>' +
-                        '<button class="btn-sm rounded-2 remove-button" style="background-color: #F4ACBC; color:white; border-color:#F4ACBC" data-friend-id="' + friend.id + '">' +
-                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">' +
-                        '<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>' +
-                        '</svg>' +
-                        '</button>' +
-                        '</div>');
+                    '<div class="d-flex align-items-center">' +
+                    '<a href="/profile/?id=' + friend.id + '" class="text-white redir">' +
+                    '<span>' + friend.username + '</span>' +
+                    '</a>' +
+                    '</div>' +
+                    '<button class="btn-sm rounded-2 remove-button" style="background-color: #F4ACBC; color:white; border-color:#F4ACBC" data-friend-id="' + friend.id + '">' +
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">' +
+                    '<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>' +
+                    '</svg>' +
+                    '</button>' +
+                    '</div>');
 
-                        $friendElement.find('.d-flex').prepend($statusIcon);
-                        $friendContainer.append($friendElement);
-                        attachEventListeners();
-                });
+                    $friendElement.find('.d-flex').prepend($statusIcon);
+                    $friendContainer.append($friendElement);
+                    });
+                    attachEventListeners();
 
                 $('.remove-button').on('click', function() {
                     var friendId = $(this).data('friend-id');
