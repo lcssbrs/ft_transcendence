de9f7f068405e08a953441e502448d56c22b4248
lcssbrs <lucas.seib@icloud.com>
2024-03-26 15:05:45 +0100

    tom correct websocket by tom

--- app/hello_django/routing.py
+++ app/hello_django/routing.py
@@ -11,7 +11,7 @@ websocket_urlpatterns = [
 ]
 
 application = ProtocolTypeRouter({
-	"http": get_asgi_application(),
+	"https": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
