9554b15217018ca16bfd84c2130880ff1365ff08
lcssbrs <lucas.seib@icloud.com>
2024-03-26 20:05:11 +0100

    try to make wss appends

--- app/hello_django/asgi.py
+++ app/hello_django/asgi.py
@@ -1,20 +1,30 @@
+"""
+ASGI config for hello_django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from hello_django.consumers import PongConsumer, ChatConsumer
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
+from hello_django import consumers
+from django.urls import re_path
 
-websocket_urlpatterns = [
-    path('wss/ranked/', PongConsumer.as_asgi()),
-    path('wss/chat/', ChatConsumer.as_asgi()),
-]
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "https": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter([
+            path("wss/ranked", consumers.PongConsumer.as_asgi()),
+			re_path(r'wss/match/(?P<match_id>\d+)/$', consumers.PongConsumer.as_asgi()),
+			path('wss/chat/', consumers.ChatConsumer.as_asgi()),
+        ])
     ),
 })
--- app/hello_django/routing.py
+++ app/hello_django/routing.py
@@ -8,21 +8,36 @@ from django.core.asgi import get_asgi_application
 websocket_urlpatterns = [
     path('wss/ranked/', PongConsumer.as_asgi()),
     path('wss/chat/', ChatConsumer.as_asgi()),
-	re_path(r'^wss/match/(?P<match_id>\d+)/$', PongConsumer.as_asgi()),
+	# re_path(r'^wss/match/(?P<match_id>\d+)/$', PongConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-    "https": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
-    ),
-})
+     "http": get_asgi_application(),
+     "websocket": AuthMiddlewareStack(
+         URLRouter(
+             websocket_urlpatterns
+         )
+     ),
+     "https": get_asgi_application(),
+     "websocket": AuthMiddlewareStack(
+         URLRouter(
+             websocket_urlpatterns
+         )
+     ),
+ })
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             websocket_urlpatterns
+#         )
+#     ),
+#     "https": AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(
+#             URLRouter(
+#                 websocket_urlpatterns
+#             )
+#         )
+#     ),
+# })
