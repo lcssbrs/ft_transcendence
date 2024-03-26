cb3d56ba0b9b06dfa3f2ed5cfada816dddc8f406
lcssbrs <lucas.seib@icloud.com>
2024-03-26 19:53:43 +0100

    try to make wss appends

--- app/hello_django/routing.py
+++ app/hello_django/routing.py
@@ -1,6 +1,6 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
+from django.urls import path, re_path
 from hello_django.consumers import PongConsumer, ChatConsumer
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
@@ -8,6 +8,7 @@ from django.core.asgi import get_asgi_application
 websocket_urlpatterns = [
     path('wss/ranked/', PongConsumer.as_asgi()),
     path('wss/chat/', ChatConsumer.as_asgi()),
+	re_path(r'^wss/match/(?P<match_id>\d+)/$', PongConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
