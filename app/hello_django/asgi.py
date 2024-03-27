09c2e64d8503d25fdfdfcd0bf45de23344dde50a
lcssbrs <lucas.seib@icloud.com>
2024-03-27 12:11:00 +0100

    try again

--- app/hello_django/asgi.py
+++ app/hello_django/asgi.py
@@ -1,30 +1,20 @@
-"""
-ASGI config for hello_django project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
-import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from hello_django import consumers
-from django.urls import re_path
+from hello_django.consumers import PongConsumer, ChatConsumer
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
+websocket_urlpatterns = [
+    path('ws/ranked/', PongConsumer.as_asgi()),
+    path('ws/chat/', ChatConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
-    "https": get_asgi_application(),
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/ranked", consumers.PongConsumer.as_asgi()),
-			re_path(r'ws/match/(?P<match_id>\d+)/$', consumers.PongConsumer.as_asgi()),
-			path('ws/chat/', consumers.ChatConsumer.as_asgi()),
-        ])
+        URLRouter(
+            websocket_urlpatterns
+        )
     ),
 })
--- app/hello_django/routing.py
+++ app/hello_django/routing.py
@@ -1,28 +1,17 @@
+from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
 from hello_django.consumers import PongConsumer, ChatConsumer
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 websocket_urlpatterns = [
     path('ws/ranked/', PongConsumer.as_asgi()),
     path('ws/chat/', ChatConsumer.as_asgi()),
-    # Vous pouvez ajouter d'autres patterns ici si nécessaire
 ]
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
     ),
-    # "https": AllowedHostsOriginValidator(
-    #     AuthMiddlewareStack(
-    #         URLRouter(
-    #             websocket_urlpatterns
-    #         )
-    #     )
-    # ),
 })
