e38a4a10ff137b081c151b9426a09fae1c205add
lcssbrs <lucas.seib@icloud.com>
2024-03-27 16:31:03 +0100

    change ranked

--- app/static/js/ranked.js
+++ app/static/js/ranked.js
@@ -492,8 +492,7 @@ function setupRanked() {
 		}
 
 		document.addEventListener('keydown', function(event) {
-			if (gameStarted && playerId == 1) {
-				game.ball.x = 50; // rm ici
+			if (gameStarted) {
 				if (event.key === 'w' || event.key === 'W' || event.key === 'z' || event.key === 'Z') {
 					updatePad('up')
 					sendGameMove(playerId, 'up');
