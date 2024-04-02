a0de2f438f99bb886cfb04d9c8f9e04b662093a0
lcssbrs <lucas.seib@icloud.com>
2024-04-02 11:58:21 +0200

    finish merge

--- app/hello_django/routing.py
+++ app/hello_django/routing.py
@@ -1,14 +1,6 @@
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-<<<<<<< HEAD
-from hello_django.consumers import PongConsumer, ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/ranked/', PongConsumer.as_asgi()),
-    path('ws/chat/', ChatConsumer.as_asgi()),
-    path('ws/match/<match_id>/', PongConsumer.as_asgi()),
-=======
 from django.urls import path
 from hello_django.consumers import PongConsumer, ChatConsumer, TournamentConsumer
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -18,7 +10,6 @@ websocket_urlpatterns = [
     path('ws/ranked', PongConsumer.as_asgi()),
 	path('ws/chat/', ChatConsumer.as_asgi()),
     path('ws/tournament', TournamentConsumer.as_asgi()),
->>>>>>> tom
 ]
 
 application = ProtocolTypeRouter({
