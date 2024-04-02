063341fd432ff92e409c0f7a8e875f452944ad30
lcssbrs <lucas.seib@icloud.com>
2024-04-02 15:18:07 +0200

    ranked not working

--- app/static/js/online.js
+++ app/static/js/online.js
@@ -6,7 +6,7 @@ function online() {
 	let socket = null;
 
 	var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
-	var ws_path = ws_scheme + '://' + `root.alan-andrieux/ws/chat/`;
+	var ws_path = ws_scheme + '://' + `root.alan-andrieux.fr/ws/chat/`;
 
 	socket = new WebSocket(ws_path);
 
