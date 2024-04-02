72f60d6019923ac59aad8da802ba85fc764c4fb8
lcssbrs <lucas.seib@icloud.com>
2024-04-02 12:10:37 +0200

    finish merge

--- app/hello_django/asgi.py
+++ app/hello_django/asgi.py
@@ -1,15 +1,22 @@
+"""
+ASGI config for hello_django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from hello_django.consumers import PongConsumer, ChatConsumer
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-from . import consumers
+from hello_django import consumers
+from django.urls import re_path
 
-websocket_urlpatterns = [
-    path('ws/ranked/', PongConsumer.as_asgi()),
-    path('ws/chat/', ChatConsumer.as_asgi()),
-]
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
